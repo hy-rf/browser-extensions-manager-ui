17a0fc5ace4cd0e3200061b1fbbcbfb99733338a
hy-rf <heyu880202@gmail.com>
2025-04-03 21:45:20 +0800

    feat: sort by state

--- index.js
+++ index.js
@@ -11,3 +11,66 @@ document.addEventListener("DOMContentLoaded", () => {
     })
     .catch((error) => console.error("Error fetching the JSON data:", error));
 });
+
+document
+  .getElementsByClassName("sort-buttons")[0]
+  .addEventListener("click", showAll);
+
+document
+  .getElementsByClassName("sort-buttons")[1]
+  .addEventListener("click", showActive);
+
+document
+  .getElementsByClassName("sort-buttons")[2]
+  .addEventListener("click", showInactive);
+
+function removeAll() {
+  const dataList = document.getElementById("data-list");
+  dataList.innerHTML = "";
+}
+
+function showAll() {
+  fetch("data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const dataList = document.getElementById("data-list");
+      data.forEach((item) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = `Name: ${item.name}, Age: ${item.description}`;
+        dataList.appendChild(listItem);
+      });
+    })
+    .catch((error) => console.error("Error fetching the JSON data:", error));
+}
+
+function showActive() {
+  fetch("data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const dataList = document.getElementById("data-list");
+      data.forEach((item) => {
+        if (item.isActive) {
+          const listItem = document.createElement("li");
+          listItem.textContent = `Name: ${item.name}, Age: ${item.description}`;
+          dataList.appendChild(listItem);
+        }
+      });
+    })
+    .catch((error) => console.error("Error fetching the JSON data:", error));
+}
+
+function showInactive() {
+  fetch("data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const dataList = document.getElementById("data-list");
+      data.forEach((item) => {
+        if (!item.isActive) {
+          const listItem = document.createElement("li");
+          listItem.textContent = `Name: ${item.name}, Age: ${item.description}`;
+          dataList.appendChild(listItem);
+        }
+      });
+    })
+    .catch((error) => console.error("Error fetching the JSON data:", error));
+}
